02912fda66c411a3d71b122ff7da02723b8f5407
Joel Scott <jscottusf@gmail.com>
2020-04-14 23:53:10 -0400

    adding some architechture

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+var logger = require("morgan");
+const exphbs = require("express-handlebars");
+const db = require("./models");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+//middleware
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//set up handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+let URI = process.env.MONGODB_URI || "mongodb://localhost/npr_scraper";
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+});
+
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, function () {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
+});
